45a8143003b6a9476426547c162f6372ad246874
don <udochuchukwudavid8@gmail>
2024-10-10 15:05:44 +0100

    update on helloworld

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import javax.swing.JOptionPane;
 
 class helloworld {
     public static void main(String[] args) {
-//        TimeService timeService = new TimeService();
-//        while(true){
-//            int input = Integer.parseInt(JOptionPane.showInputDialog("Pick one of the options. 0 or 1. O to convert from Days to Hour, 1 to convert from week to Hours"));
-//            if (input == 0) {
-//                timeService.getDaysToHours();
-//            }
-//            else if (input == 1) {
-//                timeService.getWeeksToHours();
-//            }
-//        }
+        TimeService timeService = new TimeService();
+        while(true){
+            int input = Integer.parseInt(JOptionPane.showInputDialog("Pick one of the options. 0 or 1. O to convert from Days to Hour, 1 to convert from week to Hours"));
+            if (input == 0) {
+                timeService.getDaysToHours();
+            }
+            else if (input == 1) {
+                timeService.getWeeksToHours();
+            }
+            System.out.println("Hello world trial");
+            System.out.println("Hello world trial for git hub");
+            System.out.println(4*8);
+        }
 
-        System.out.println("Hello world trial loloooooooooo");
     }
  }
